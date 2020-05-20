ec05b0012693b044a2dae2260eeb9bbd5f3c7a31
eliranbar <eliran.bar@gmail.com>
2020-05-20 14:59:41 +0300

    add travis

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Eliran bar jjjjj
+          Learn Eliran bar jjjjjeeeeeS
         </a>
       </header>
     </div>
